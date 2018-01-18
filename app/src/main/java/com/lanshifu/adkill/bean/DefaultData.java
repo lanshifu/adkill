0bd03edabe4840a611070b0dfda9bdb543a138a9
lanshifu <1101460241@qq.com>
2018-01-19 00:38:50 +0800

    自定义

--- app/src/main/java/com/lanshifu/adkill/bean/DefaultData.java
+++ app/src/main/java/com/lanshifu/adkill/bean/DefaultData.java
@@ -9,16 +9,16 @@ public class DefaultData {
     public static String text = "跳";
     public static String[] appNames = new String[]{
             "今日头条",
-            "今日头条中间广告",
             "广东移动",
+            "网易云中间广告",
             "网易云音乐",
             "ofo共享单车",
             "UC",
     };
     public static String[] firstActivitys = new String[]{
             "com.ss.android.article.news.activity.SplashBadgeActivity",
-            "com.ss.android.article.news.activity.LoadingAdActivity",
             "com.kingpoint.gmcchh.newui.main.skeleton.view.SkeletonActivity",
+            "com.netease.cloudmusic.activity.LoadingAdActivity",
             "com.netease.cloudmusic.activity.LoadingActivity",
             "so.ofo.labofo.activities.EntryActivity",
             "first:com.UCMobile.main.UCMobile",
